5c335aabbebcd5b58c18729aae61c13305103f8a
Zack Jordan <pixelwelders@gmail.com>
2014-11-14 13:02:34 -0600

    Added a bunch more quotes.

--- Source/inspire365.js
+++ Source/inspire365.js
@@ -2,28 +2,7 @@
  * Created by Zack Jordan on 11/14/14.
  */
 
-var quotes = [
-    {
-        quote:"It is our choices that show what we truly are, far more than our abilities.",
-        author:"Albus Dumbledore"
-    },
-    {
-        quote:"Anyone who has never made a mistake has never tried anything new.",
-        author:"Albert Einstein"
-    },
-    {
-        quote:"If you don't know where you are going, you'll end up someplace else.",
-        author:"Yogi Berra"
-    },
-    {
-        quote:"A wise man will make more opportunities than he finds.",
-        author:"Francis Bacon"
-    },
-    {
-        quote:"The trouble with most of us is that we would rather be ruined by praise than saved by criticism.",
-        author:"Norman Vincent Peale"
-    }
-];
+var quotes = quotes.quotes;
 
 function $(id) {
     return document.getElementById(id);
---
+++ Source/quotes.js
@@ -0,0 +1,405 @@
+var quotes = {
+    "version": "1.0",
+    "quotes": [
+        {
+            "quote": "Don't judge each day by the harvest you reap but by the seeds that you plant. ",
+            "author": "Robert Louis Stevenson"
+        },
+        {
+            "quote": "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree. ",
+            "author": "Martin Luther"
+        },
+        {
+            "quote": "I hated every minute of training, but I said to myself, 'Don't quit. Suffer now and live the rest of your life as a champion.'",
+            "author": "Muhammad Ali"
+        },
+        {
+            "quote": "Someone's sitting in the shade today because someone planted a tree a long time ago. ",
+            "author": "Warren Buffett"
+        },
+        {
+            "quote": "Cherish your visions and your dreams as they are the children of your soul, the blueprints of your ultimate achievements. ",
+            "author": "Napoleon Hill"
+        },
+        {
+            "quote": "Thinking: the talking of the soul with itself. ",
+            "author": "Plato"
+        },
+        {
+            "quote": "What great thing would you attempt if you knew you could not fail?",
+            "author": "Robert H. Schuller"
+        },
+        {
+            "quote": "Enthusiasm moves the world. ",
+            "author": "Arthur Balfour"
+        },
+        {
+            "quote": "It is always the simple that produces the marvelous. ",
+            "author": "Amelia Barr"
+        },
+        {
+            "quote": "The power of imagination makes us infinite. ",
+            "author": "John Muir"
+        },
+        {
+            "quote": "I am deliberate and afraid of nothing. ",
+            "author": "Audre Lorde"
+        },
+        {
+            "quote": "Let each man exercise the art he knows. ",
+            "author": "Aristophanes"
+        },
+        {
+            "quote": "Men must live and create. Live to the point of tears. ",
+            "author": "Albert Camus"
+        },
+        {
+            "quote": "The only thing that ultimately matters is to eat an ice cream cone, play a slide trombone, plant a small tree, good God, now you're free. ",
+            "author": "Ray Manzarek"
+        },
+        {
+            "quote": "The meaning I picked, the one that changed my life: overcome fear, behold wonder. ",
+            "author": "Richard Bach"
+        },
+        {
+            "quote": "Ideas shape the course of history. ",
+            "author": "John Maynard Keynes"
+        },
+        {
+            "quote": "It's inspirational to see someone who's dying smile. ",
+            "author": "Arlen Specter"
+        },
+        {
+            "quote": "Try not to become a man of success but a man of value. ",
+            "author": "Albert Einstein"
+        },
+        {
+            "quote": "If you have built castles in the air, your work need not be lost; that is where they should be. Now put foundations under them. ",
+            "author": "Henry David Thoreau"
+        },
+        {
+            "quote": "Inspiration and genius--one and the same. ",
+            "author": "Victor Hugo "
+        },
+        {
+            "quote": "If you would create something, you must be something. ",
+            "author": "Johann Wolfgang von Goethe"
+        },
+        {
+            "quote": "Every artist was first an amateur. ",
+            "author": "Ralph Waldo Emerson"
+        },
+        {
+            "quote": "Don't wait for extraordinary opportunities. Seize common occasions and make them great. ",
+            "author": "Orison Swett Marden"
+        },
+        {
+            "quote": "No great man ever complains of want of opportunities. ",
+            "author": "Ralph Waldo Emerson "
+        },
+        {
+            "quote": "Men do less than they ought, unless they do all they can. ",
+            "author": "Thomas Carlyle"
+        },
+        {
+            "quote": "Let thy words be few. ",
+            "author": "Ecclesiastes 5:2"
+        },
+        {
+            "quote": "Happy are those who dream dreams and are willing to pay the price to make them come true. ",
+            "author": "Leon J. Suenes"
+        },
+        {
+            "quote": "First say to yourself what you would be; and then do what you have to do. ",
+            "author": "Epictetus"
+        },
+        {
+            "quote": "You miss 100% of the shots you don't take. ",
+            "author": "Wayne Gretsky"
+        },
+        {
+            "quote": "The ability to convert ideas to things is the secret to outward success.",
+            "author": "Henry Ward Beecher"
+        },
+        {
+            "quote": "The ability to concentrate and to use your time well is everything if you want to succeed in business--or almost anywhere else for that matter.",
+            "author": "Lee Iacocca"
+        },
+        {
+            "quote": "A wise man will make more opportunities than he finds.",
+            "author": "Francis Bacon"
+        },
+        {
+            "quote": "In everything the ends well defined are the secret of durable success.",
+            "author": "Victor Cousins"
+        },
+        {
+            "quote": "The great and glorious masterpiece of man is to know how to live to purpose.",
+            "author": "Michel de Montaigne"
+        },
+        {
+            "quote": "The great and glorious masterpiece of man is to know how to live to purpose.",
+            "author": "Khalil Gibran"
+        },
+        {
+            "quote": "Every ceiling, when reached, becomes a floor, upon which one walks as a matter of course and prescriptive right.",
+            "author": "Aldous Huxley"
+        },
+        {
+            "quote": "If you don't know where you are going, you'll end up someplace else.",
+            "author": "Yogi Berra"
+        },
+        {
+            "quote": "Life can be pulled by goals just as surely as it can be pushed by drives.",
+            "author": "Viktor Frankl"
+        },
+        {
+            "quote": "The virtue lies in the struggle, not in the prize.",
+            "author": "Richard Monckton Milnes"
+        },
+        {
+            "quote": "Our plans miscarry because they have no aim. When a man does not know what harbor he is making for, no wind is the right wind.",
+            "author": "Seneca"
+        },
+        {
+            "quote": "It is not enough to take steps which may some day lead to a goal; each step must be itself a goal and a step likewise.",
+            "author": "Johann Wolfgang von Goethe"
+        },
+        {
+            "quote": "In absence of clearly defined goals, we become strangely loyal to performing daily acts of trivia.",
+            "author": "Anonymous"
+        },
+        {
+            "quote": "We are what we repeatedly do. Excellence, therefore, is not an act but a habit.",
+            "author": "Aristotle"
+        },
+        {
+            "quote": "Take calculated risks.  That is quite different from being rash.",
+            "author": "George S. Patton"
+        },
+        {
+            "quote": "We are all inventors, each sailing out on a voyage of discovery, guided each by a private chart, of which there is no duplicate. The world is all gates, all opportunities. ",
+            "author": "Ralph Waldo Emerson "
+        },
+        {
+            "quote": "Seek the lofty by reading, hearing and seeing great work at some moment every day.",
+            "author": "Thornton Wilder"
+        },
+        {
+            "quote": "Without inspiration the best powers of the mind remain dormant. There is a fuel in us which needs to be ignited with sparks.",
+            "author": "Johann Gottfried von Herder"
+        },
+        {
+            "quote": "And all may do what has by man been done.",
+            "author": "Edward Young"
+        },
+        {
+            "quote": "If the wind will not serve, take to the oars.",
+            "author": "Latin Proverb"
+        },
+        {
+            "quote": "Men's best successes come after their disappointments. ",
+            "author": "Henry Ward Beecher"
+        },
+        {
+            "quote": "Do not wait to strike till the iron is hot; but make it hot by striking.",
+            "author": "William B. Sprague"
+        },
+        {
+            "quote": "Nothing will ever be attempted if all possible objections must first be overcome.",
+            "author": "Samuel Johnson"
+        },
+        {
+            "quote": "Fortune favors the brave.",
+            "author": "Publius Terence"
+        },
+        {
+            "quote": "An oak is not felled at one blow. ",
+            "author": "Spanish Proverb"
+        },
+        {
+            "quote": "Great spirits have always encountered violent opposition from mediocre minds.",
+            "author": "Albert Einstein "
+        },
+        {
+            "quote": "Believe with all of your heart that you will do what you were made to do.",
+            "author": "Orison Swett Marden"
+        },
+        {
+            "quote": "Knowing is not enough; we must apply. Willing is not enough; we must do.",
+            "author": "Johann Gottfried von Goethe"
+        },
+        {
+            "quote": "Nothing great was ever achieved without enthusiasm.",
+            "author": "Ralph Waldo Emerson"
+        },
+        {
+            "quote": "I failed my way to success.",
+            "author": "Thomas Edison "
+        },
+        {
+            "quote": "Our doubts are traitors, and make us lose the good we oft might win, by fearing to attempt.",
+            "author": "William Shakespeare "
+        },
+        {
+            "quote": "I was never afraid of failure, for I would sooner fail than not be among the best.",
+            "author": "John Keats "
+        },
+        {
+            "quote": "It is foolish to fear what you cannot avoid.",
+            "author": "Publius Syrus"
+        },
+        {
+            "quote": "One who fears failure limits his activities.  Failure is only the opportunity to more  intelligently begin again.",
+            "author": "Henry Ford"
+        },
+        {
+            "quote": "The greatest mistake you can make in life is to continually be afraid you will make one.",
+            "author": "Elbert Hubbard "
+        },
+        {
+            "quote": "Little minds are tamed and subdued by misfortunes; but great minds rise above them.",
+            "author": "Washington Irving"
+        },
+        {
+            "quote": "Our greatest glory consist not in never falling, but in rising every time we fall.",
+            "author": "Oliver Goldsmith"
+        },
+        {
+            "quote": "Wherever we look upon this earth, the opportunities take shape within the problems.",
+            "author": "Nelson A. Rockefeller"
+        },
+        {
+            "quote": "What would life be if we had no courage to attempt anything?",
+            "author": "Vincent van Gogh "
+        },
+        {
+            "quote": "Experience teaches slowly, and at the cost of mistakes.",
+            "author": "James A. Froude"
+        },
+        {
+            "quote": "A man's life is interesting primarily when he has failed--I well know. For it's a sign that he tried to surpass himself.",
+            "author": "Georges Clemenceau"
+        },
+        {
+            "quote": "He who fears being conquered is sure of defeat.",
+            "author": "Napoleon Bonaparte"
+        },
+        {
+            "quote": "There is no failure except in no longer trying.",
+            "author": "Elbert Hubbard"
+        },
+        {
+            "quote": "It is not easy to find happiness in ourselves, and it is not possible to find it elsewhere.",
+            "author": "Agnes Repplier"
+        },
+        {
+            "quote": "Know thyself means this, that you get acquainted with what you know, and what you can do.",
+            "author": "Menander"
+        },
+        {
+            "quote": "Collect as precious pearls the words of the wise and virtuous.",
+            "author": "Abd-el-Kadar"
+        },
+        {
+            "quote": "If we do not plant knowledge when young, it will give us no shade when we are old.",
+            "author": "Lord Chesterfield"
+        },
+        {
+            "quote": "Our business in life is not to get ahead of other people, but to get ahead of ourselves.",
+            "author": "Maltbie D. Matlock"
+        },
+        {
+            "quote": "If you have an hour, will you not improve that hour, instead of idling it away?",
+            "author": "Lord Chesterfield "
+        },
+        {
+            "quote": "Follow your honest convictions, and stay strong.",
+            "author": "William Thackeray"
+        },
+        {
+            "quote": "He that will not reflect is a ruined man.",
+            "author": "Asian Proverb"
+        },
+        {
+            "quote": "Every day do something that will inch you closer to a better tomorrow.",
+            "author": "Doug Firebaugh"
+        },
+        {
+            "quote": "Be not afraid of growing slowly; be afraid only of standing still.",
+            "author": "Chinese Proverb"
+        },
+        {
+            "quote": "We are either progressing or retrograding all the while; there is no such thing as remaining stationary in this life.",
+            "author": "James Freeman Clarke"
+        },
+        {
+            "quote": "To conquer oneself is the best and noblest victory; to be vanquished by one's own nature is the worst and most ignoble defeat.",
+            "author": "Plato"
+        },
+        {
+            "quote": "Everybody wants to be somebody; nobody wants to grow.",
+            "author": "Johann Wolfgang von Goethe"
+        },
+        {
+            "quote": "Change and growth take place when a person has risked himself and dares to become involved with experimenting with his own life. ",
+            "author": "Herbert Otto"
+        },
+        {
+            "quote": "Energy and persistence conquer all things.",
+            "author": "Benjamin Franklin"
+        },
+        {
+            "quote": "If we all did the things we are capable of, we would astound ourselves.",
+            "author": "Thomas Edison"
+        },
+        {
+            "quote": "A man who finds no satisfaction in himself will seek for it in vain elsewhere. ",
+            "author": "La Rouchefoucauld"
+        },
+        {
+            "quote": "Exert your talents, and distinguish yourself, and don't think of retiring from the world, until the world will be sorry that you retire.",
+            "author": "Samuel Johnson"
+        },
+        {
+            "quote": "Go confidently in the direction of your dreams. Live the life you have imagined.",
+            "author": "Henry David Thoreau"
+        },
+        {
+            "quote": "You cannot dream yourself into a character: you must hammer and forge yourself into one.",
+            "author": "Henry David Thoreau"
+        },
+        {
+            "quote": "Commitment leads to action. Action brings your dream closer. ",
+            "author": "Marcia Wieder"
+        },
+        {
+            "quote": "The best way to make your dreams come true is to wake up.",
+            "author": "Paul Valery"
+        },
+        {
+            "quote": "Imagination is more important than knowledge.",
+            "author": "Albert Einstein"
+        },
+        {
+            "quote": "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude. ",
+            "author": "Thomas Jefferson"
+        },
+        {
+            "quote": "A failure is a man who has blundered, but is not able to cash in on the experience.",
+            "author": "Elbert Hubbard"
+        },
+        {
+            "quote": "Success is the child of audacity.",
+            "author": "Benjamin Disraeli"
+        },
+        {
+            "quote": "I cannot give you the formula for success, but I can give you the formula for failure--which is: Try to please everybody.",
+            "author": "Henry Bayard Swope"
+        },
+        {
+            "quote": "The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack in will.",
+            "author": "Vince Lombardi"
+        }
+    ]
+};
